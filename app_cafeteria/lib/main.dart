14ff0619ce4f018d0fa78a9ffa66fe24be9514eb
maeda-st <77119866+witcare-developer@users.noreply.github.com>
2021-10-14 14:19:03 -0300

    Teste com FigmaToFlutter

--- app_cafeteria/lib/main.dart
+++ app_cafeteria/lib/main.dart
@@ -1,19 +1,44 @@
+// ignore_for_file: sized_box_for_whitespace, use_key_in_widget_constructors, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_svg/svg.dart';
 
-void main() {
-  runApp(const Home());
-}
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
+class BotaookWidget extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _BotaookWidgetState createState() => _BotaookWidgetState();
 }
 
-class _HomeState extends State<Home> {
+class _BotaookWidgetState extends State<BotaookWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // Figma Flutter Generator BotaookWidget - COMPONENT
+
+    return Container(
+        width: 369,
+        height: 109,
+        child: Stack(children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: SvgPicture.asset('images/retangulook.svg',
+                semanticsLabel: 'retangulook'),
+          ),
+          Positioned(
+              top: 27,
+              left: 150,
+              child: Text(
+                'OK',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    fontFamily: 'Arial',
+                    fontSize: 48,
+                    letterSpacing:
+                        0 /*percentages not used in flutter. defaulting to zero*/,
+                    fontWeight: FontWeight.normal,
+                    height: 1),
+              )),
+        ]));
   }
 }
