7d23bf39bb5a53ad663addb661615de323f301ff
Bruno <bruno.a@outlook.com.br>
2021-11-09 19:05:55 -0300

    Added popover with ID generation logic

--- menu-mobile/src/components/Popover.jsx
+++ menu-mobile/src/components/Popover.jsx
@@ -1,32 +1,73 @@
-import React, {Component, useState} from "react";
-import { Popover, Button } from 'antd';
-import {ShoppingCartOutlined, PlusOutlined, MinusOutlined} from '@ant-design/icons';
-import { InputNumber } from 'antd';
+import React, {Component} from "react";
+import "./Popover.css"
+import { Popover, Button, InputNumber, Input } from 'antd';
+import {ShoppingOutlined, ShoppingCartOutlined,PlusOutlined, MinusOutlined, CloseOutlined} from '@ant-design/icons';
 
-const  n  = 1
 
-const [currentValue, setCurrentValue] = useState(0)
-
-export default class App extends React.Component {
-  
-  onChange(value) {
-        console.log('changed', value);
+const initialState = {
+  visible:false,
+  observation:{
+    quantity: 1,
+    text:''
   }
-    
-  
-  addValue(n) {
-    
+}
+
+export default class Infos extends Component {
+
+  state = {...initialState};
+
+  constructor(props) {
+    super(props)
+    this.state = this.getInitialState();
   }
 
-  
-  state = {
-    visible: false,
-  };
+  getInitialState = () => ({
+    visible:false,
+    observation:{
+      id: '',
+      text:'',
+      quantity: 1,
+      
+    }
+  })
+
+  GetId(){
+    let min = 1;
+    let max = 10000;
+    let rand = Math.round(min+ (Math.random()* (max-min)))
+    return rand
+  }
+
+  addValue() {
+    const obs = {...this.state.observation}
+    obs.quantity =  this.state.observation.quantity++
+    return this.setState({obs})
+  }
+
+  RemoveValue(){
+    const obs = {...this.state.observation}
+    obs.quantity =  this.state.observation.quantity--
+    return this.setState({obs})
+  }
+
+  UpdateField(event) {
+    let field = event.target.name;
+    let value = event.target.value;
+    this.state.observation[field] = value
+    return this.setState({observation:this.state.observation})
+  }
+
+  ReturnRequest(){
+    const a = {...this.state.observation}
+    a.id = this.GetId()
+    console.log(a)
+    this.setState(this.getInitialState())
+
+  }
+
 
   hide = () => {
-    this.setState({
-      visible: false,
-    });
+    this.setState(this.getInitialState());
   };
 
   handleVisibleChange = visible => {
@@ -37,20 +78,40 @@ export default class App extends React.Component {
     return (
       <Popover
         content={
-            <div>
-              <MinusOutlined></MinusOutlined>
-                <InputNumber min={1} max={10} defaultValue={n} onChange={this.onChange} />
-                <PlusOutlined></PlusOutlined>
-                <a onClick={this.hide}>Close</a>
+          <div className='div-popover'>
+            <div className='div-text' >
+            <Input placeholder='Obsrvações do pedido' size={'middle'} value={this.state.observation.text} 
+            onChange={e => this.UpdateField(e)} name='text' />
+            </div>
+            <div className='div-number'>
+              <button onClick={e => this.RemoveValue(e)} >
+                <MinusOutlined></MinusOutlined>
+              </button>
+              <InputNumber min={1} max={100}  
+              value={this.state.observation.quantity} size={'small'} 
+              onChange={e => this.UpdateField(e)} name='quantity'/>
+              <button onClick={e => this.addValue(e)} >
+                <PlusOutlined ></PlusOutlined>
+              </button>
+            </div>
+            <div className='div-button'>
+              <Button type="primary" style={{backgroundColor:'#532B06', border:'none'}}
+                shape="round" icon={<ShoppingCartOutlined />} size={"middle"} onClick={this.ReturnRequest.bind(this)}>
+              </Button>
+              <Button type="primary" style={{backgroundColor:'#532B06', border:'none'}}
+                shape="round" icon={<CloseOutlined />} size={"middle"} onClick={this.hide} >
+              </Button>
             </div>
 
-    }
-        title="Title"
+
+          </div>
+        }
+        title="Adicionar ao Pedido"
         trigger="click"
         visible={this.state.visible}
         onVisibleChange={this.handleVisibleChange}
       >
-        <Button type="primary" style={{backgroundColor:'#532B06', border:'none'}} shape="round" icon={<ShoppingCartOutlined />} size={"middle"} />
+        <Button type="primary" style={{backgroundColor:'#532B06', border:'none'}} shape="round" icon={<ShoppingOutlined />} size={"middle"} />
       </Popover>
     );
   }
